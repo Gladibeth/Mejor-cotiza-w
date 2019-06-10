04516bc14665779ac5b44531f6256893d84d02fb
Gladibeth <gladibethdelos@gmail>
2019-06-09 22:36:26 -0400

    first commit

---
+++ docs/assets/js/setting-slick.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+  $(".single-item").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    infinite: true,
+    cssEase: 'linear',
+    fade: true,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+
+        }
+      }
+    ]
+  });
+  $('.content-categories').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 1000
+        }
+      }
+    ]
+  });
+
+  $('.slider-professional').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 1000
+        }
+      }
+    ]
+  });
+
+});
+
+
