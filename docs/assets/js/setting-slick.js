a8a94e0b89f685e5a0bd608aed463126892367b4
Gladibeth <gladibethdelos@gmail>
2019-06-12 17:09:22 -0400

    Add sections in view index

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -92,6 +92,43 @@ $(document).ready(function () {
     ]
   });
 
+  $('.content-partnerts').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 1000
+        }
+      }
+    ]
+  });
+
 });
 
 
