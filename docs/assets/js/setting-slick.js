52a89b982bdfbdeb258a761aade48c3a00808ea3
Gladibeth <gladibethdelos@gmail>
2019-06-23 23:18:09 -0400

    Add seccion team and small size partnets

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -144,6 +144,41 @@ $(document).ready(function () {
     dots: true,
     focusOnSelect: true
   });
+  $('.team-carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 1000
+        }
+      }
+    ]
+  });
 
 });
 
