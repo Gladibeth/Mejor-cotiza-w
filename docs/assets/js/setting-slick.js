8c2aa7f94f5cb3ca665b7198e16a75760bfe6716
Gladibeth <gladibethdelos@gmail>
2019-06-24 17:15:46 -0400

    Add form work

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -58,6 +58,43 @@ $(document).ready(function () {
 
   $('.slider-professional').slick({
     infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: true,
+          arrows: false,
+          autoplaySpeed: 1000
+        }
+      }
+    ]
+  });
+
+  $('.slider-bidding').slick({
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: false,
